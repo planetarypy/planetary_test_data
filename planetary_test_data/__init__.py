96c2f947e17bcfc148386234e2cea15c1104b8d5
Austin Godber <godber@uberhip.com>
2015-07-21 13:43:47 -0700

    updated metainfo for release

--- planetary_test_data/__init__.py
+++ planetary_test_data/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'PlanetaryPy Developers'
 __email__ = 'contact@planetarypy.com'
-__version__ = '0.1.0'
+__version__ = '0.3.2'
 
 
 from .planetary_test_data import PlanetaryTestDataProducts
