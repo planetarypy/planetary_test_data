a68c1d396de399e9424b24a89099524f24116b22
Austin Godber <godber@uberhip.com>
2015-07-27 14:17:21 -0700

    updating version for new release

--- planetary_test_data/__init__.py
+++ planetary_test_data/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'PlanetaryPy Developers'
 __email__ = 'contact@planetarypy.com'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 
 from .planetary_test_data import PlanetaryTestDataProducts
